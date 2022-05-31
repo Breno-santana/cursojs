58dc716ebfb9a76bd587aeace08e3c4762004602
Breno santana <93136785+Breno-santana@users.noreply.github.com>
2022-05-31 20:09:07 -0300

    Update script.js

--- ex018/script.js
+++ ex018/script.js
@@ -1,10 +1,19 @@
 let inp = document.getElementById('num');
-let sele = document.getElementById('analise');
+let sele = document.querySelector('option');
 let btn1 = document.getElementById('btn-1'); 
 let btn2 = document.getElementById('btn-2');
 let res = document.querySelector('.saida');
 let corpo = document.querySelector('body');
 
-btn1.addEventListener('click', adicionar = () =>{});
+btn1.addEventListener('click', adicionar = () =>{
+    if(inp.value.length == 0){
+        alert('Nenhum número digitado. Adicione um valor.');
+    }else{
+        let numero = Number(inp.value);
+        let item = document.createElement('option');
+        item.innerHTML = numero;
+        sele.appendChild(item);
+    }
+});
 
 btn2.addEventListener('click', finalizar = () =>{});
