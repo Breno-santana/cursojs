9712d9a94fbd0bc73a37ad1962662faf95c8c303
Breno santana <93136785+Breno-santana@users.noreply.github.com>
2022-05-31 15:04:38 -0300

    Update script.js

--- ex018/script.js
+++ ex018/script.js
@@ -3,14 +3,8 @@ let sele = document.getElementById('analise');
 let btn1 = document.getElementById('btn-1'); 
 let btn2 = document.getElementById('btn-2');
 let res = document.querySelector('.saida');
-let
+let corpo = document.querySelector('body');
 
-//btn1.addEventListener('click', adicionar = () =>{});
+btn1.addEventListener('click', adicionar = () =>{});
 
-btn2.addEventListener('click', finalizar = () =>{
-    alert('todo ok');
-    //res.innerHTML = 'gfgfgfhgh';
-    //section.style.backgroundColor='orange';
-});
-
-
+btn2.addEventListener('click', finalizar = () =>{});
