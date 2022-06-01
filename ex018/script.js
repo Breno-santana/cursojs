b7298348cb0846048c3c38ebbc205569e640f7a5
Breno santana <93136785+Breno-santana@users.noreply.github.com>
2022-06-01 11:18:53 -0300

    Update script.js

--- ex018/script.js
+++ ex018/script.js
@@ -3,7 +3,6 @@ let sele = document.querySelector('option');
 let btn1 = document.getElementById('btn-1'); 
 let btn2 = document.getElementById('btn-2');
 let res = document.querySelector('.saida');
-let corpo = document.querySelector('body');
 
 btn1.addEventListener('click', adicionar = () =>{
     if(inp.value.length == 0){
@@ -11,8 +10,8 @@ btn1.addEventListener('click', adicionar = () =>{
     }else{
         let numero = Number(inp.value);
         let item = document.createElement('option');
-        item.innerHTML = numero;
-        sele.appendChild(item);
+        sele.appendChild(item).innerHTML = `Vetor ${numero} adicionado.`;
+        sele.style.padding='0rem 0.5rem';
     }
 });
 
